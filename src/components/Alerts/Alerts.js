b848b21e9ece4bc7e92b49ca1e71f7b80c957adc
Anton Golovachov <antongol@nextbi.ru>
2017-10-20 15:14:32 +0300

    Alerts prop-types

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -1,48 +1,49 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import {array, func} from 'prop-types';
 import NotificationSystem from 'react-notification-system';
 import './Alerts.css';
 
+
 export default class Alerts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this._notificationSystem = null;
-    }
-
-    static propTypes = {
-        alerts: PropTypes.array,
-        onShow: PropTypes.func
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.alerts !== this.props.alerts) {
-            this.addToasts(this.props);
-        }
-    }
-
-    componentDidMount = () => {
-        this._notificationSystem = this.refs.notificationSystem;
-
-    }
-    addToasts = (response) => {
-        let alerts = response.alerts ? response.alerts : response;
-        if (!alerts) {
-            return;
-        }
-        alerts.map(v => this.addToast(v.message, v.level));
-    };
-
-    addToast = (message, level = "info", position = 'tc') => { // level = success, error, warning, info
-        this._notificationSystem.addNotification({
-            message,
-            level,
-            position
-        });
-        this.props.onShow();
-    };
-
-    render() {
-        return <NotificationSystem ref="notificationSystem" />;
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this._notificationSystem = null;
+	}
+	
+	componentDidUpdate(prevProps) {
+		if (prevProps.alerts !== this.props.alerts) {
+			this.addToasts(this.props);
+		}
+	}
+	
+	componentDidMount() {
+		this._notificationSystem = this.refs.notificationSystem;
+	}
+	
+	addToasts = (response) => {
+		let alerts = response.alerts ? response.alerts : response;
+		if (!alerts) {
+			return;
+		}
+		alerts.map(v => this.addToast(v.message, v.level));
+	};
+	
+	addToast = (message, level = "info", position = 'tc') => { // level = success, error, warning, info
+		this._notificationSystem.addNotification({
+			message,
+			level,
+			position
+		});
+		this.props.onShow();
+	};
+	
+	render() {
+		return <NotificationSystem ref="notificationSystem"/>;
+	}
 }
+
+Alerts.propTypes = {
+	alerts: array,
+	onShow: func
+};
